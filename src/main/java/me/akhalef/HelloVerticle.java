90f951b7162cdab270bf8611684cc2f08da84367
KhalefAhmed <khalef.ahmedkhalil@gmail.com>
2026-05-15 00:21:01 +0200

    Refactor hello.vertx.address consumer to handle empty messages and improve logging

--- src/main/java/me/akhalef/HelloVerticle.java
+++ src/main/java/me/akhalef/HelloVerticle.java
@@ -6,14 +6,16 @@ public class HelloVerticle extends AbstractVerticle {
 
     @Override
     public void start() {
+        // Single consumer for the address. Decide reply based on the message body.
         vertx.eventBus().consumer("hello.vertx.address", msg -> {
-            System.out.println("Received message: " + msg.body());
-            msg.reply("Hello from Vert.x!");
-        });
-
-        vertx.eventBus().consumer("hello.vertx.address", msg -> {
-            String name = msg.body().toString();
-            msg.reply(String.format("Hello, %s!", name));
+            Object body = msg.body();
+            System.out.println("Received message on hello.vertx.address: " + body);
+            if (body == null || body.toString().isEmpty()) {
+                msg.reply("Hello from Vert.x!");
+            } else {
+                String name = body.toString();
+                msg.reply(String.format("Hello, %s!", name));
+            }
         });
     }
 }
