33ea204089925dbbf45025f8e5ea7978d02b70a0
KhalefAhmed <khalef.ahmedkhalil@gmail.com>
2026-05-14 23:56:29 +0200

    Implement REST API endpoints for greeting in MainVerticle and add vertx-web dependency

--- src/main/java/me/akhalef/MainVerticle.java
+++ src/main/java/me/akhalef/MainVerticle.java
@@ -1,13 +1,24 @@
 package me.akhalef;
 
 import io.vertx.core.AbstractVerticle;
+import io.vertx.ext.web.Router;
 
 public class MainVerticle extends AbstractVerticle {
 
     @Override
     public void start() {
-        vertx.createHttpServer().requestHandler(request -> {
-            request.response().end("Hello Vertx World!");
-        }).listen(8080);
+        Router router = Router.router(vertx);
+        router.get("/api/v1/hello")
+                .handler(ctx -> ctx.request().response()
+                        .putHeader("content-type", "text/plain")
+                        .end("Hello, World!"));
+
+        router.get("/api/v1/hello/:name")
+                .handler(ctx -> ctx.request().response().putHeader("content-type", "text/plain")
+                        .end("Hello, " + ctx.request().getParam("name") + "!"));
+
+        vertx.createHttpServer()
+                .requestHandler(router)
+                .listen(8080);
     }
 }
